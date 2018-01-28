80323fbac62e72dca7cb40fc03a485ebbce0efa2
Stefan Seeland <stefan.seeland@gmx.at>
2018-01-28 20:09:27 +0100

    changed - now automatedMediaterm.sh resumes a aborted download
    changed - using log file in home directory (i use this script unattended)
    changed - minimum film list age is 1440min / 1 day before it gets updated

--- automatedMediaterm.sh
+++ automatedMediaterm.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 cd ~/MediaTerm/
-#updated list avaliable videos
-./mediaterm.sh -uq
+PROBLEMLOGFILE="/home/htpc/mediaterm.log"
+#updated list avaliable videos - if databse is older than one day
+if test `find "filmliste" -mmin +1440`
+then
+    echo film list is older than one day - updating | tee -a "$PROBLEMLOGFILE"
+    ./mediaterm.sh -uq | tee -a "$PROBLEMLOGFILE"
+else
+ echo film list is younger than one day | tee -a "$PROBLEMLOGFILE"
+fi
 
 #get all avaliable science busters episodes from orf servers (==apa.at)
 ./mediaterm.sh -sngow "Science Busters" | grep apa.at | grep SCIENCE-BUSTERS | while read line
 do
 	#download found file if there is no file with the same name in the destination folder
-       	wget --limit-rate 600k -nc $line -P ~/Videos/mediaterm/
+        echo downloding $line | tee -a "$PROBLEMLOGFILE"
+       	wget -c --limit-rate 600k $line -P ~/Videos/mediaterm/  | tee -a "$PROBLEMLOGFILE"
+        echo downloding $line done | tee -a "$PROBLEMLOGFILE"
 done
 
 ./mediaterm.sh -sngow "Kurzschluss - " | grep http | while read line
 do
-	
 	IFS='|' read -a myarray <<< "$line"
         #download found file if there is no file with the same name in the destination folder
-        echo downalodaing ${myarray[0]}
-	wget --limit-rate 600k -nc ${myarray[0]} -P ~/Videos/mediaterm/Kurzschluss/
+        echo downloding ${myarray[0]} | tee -a "$PROBLEMLOGFILE"
+	wget -c --limit-rate 600k ${myarray[0]} -P ~/Videos/mediaterm/Kurzschluss/ | tee -a "$PROBLEMLOGFILE"
+	echo downloding ${myarray[0]} done | tee -a "$PROBLEMLOGFILE"
 done
 
